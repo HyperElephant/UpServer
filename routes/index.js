02d02239661eb516853b98322e7d9468835366c8
Alexander Bahl <xanderbahl@gmail.com>
2016-09-15 23:12:52 -0400

    project setup
    Signed-off-by: Alexander Bahl <xanderbahl@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require('firebase');
+
+
+firebase.initializeApp({
+  serviceAccount: "serviceAccountCredentials.json",
+  databaseURL: "https://you-up-59f97.firebaseio.com/"
+});
+
+// Get a database reference to our posts
+var db = firebase.database();
+var ref = db.ref("ups");
+
+// Attach an asynchronous callback to read the data at our reference
+ref.on("value", function(snapshot) {
+  console.log(snapshot.val());
+  setTimeout(evaluatePush(snapshot), 30000);
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+
+function evaluatePush(snapshot) {
+  if (snapshot.child("seen") == false) {
+    push(snapshot);
+  }
+}
+
+function push(snapshot){
+  console.log("Pushed: " + snapshot.key);
+}
+
 
 /* GET home page. */
 
